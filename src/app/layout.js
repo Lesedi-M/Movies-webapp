8590787161150f8691eebe5f0e867c947e725e91
Lesedi <lesedimautsoa2@gmail.com>
2024-04-01 15:16:31 +0200

    added search functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "./Providers";
+import SearchBox from "@/components/SearchBox";
 
 import NavBar from "@/components/NavBar";
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         <Providers  >
           <Header/>
           <NavBar/>
+          <SearchBox/>
           {children}
         </Providers>
         </body>
